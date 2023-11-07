b77bafde2cd9cde8cee3b8a428b4838f428b3001
henri <127192852+32henri@users.noreply.github.com>
2023-11-07 10:48:39 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,37 @@
+let shopItemsData = [
+    {
+        id: "a",
+        name: "Eesti kartul",
+        price: 2,
+        desc: "Eesti talu kartul(hind on kilo kohta).",
+        img: "imagelink"
+    },
+    {
+        id: "b",
+        name: "Läti kartul",
+        price: 1,
+        desc: "Läti talu kartul(hind on kilo kohta).",
+        img: "imagelink"
+    },
+    {
+        id: "c",
+        name: "Briti kartul",
+        price: 2,
+        desc: "Briti talu kartul(hind on kilo kohta).",
+        img: "imagelink"
+    },
+    {
+        id: "d",
+        name: "Hiina kartul",
+        price: 2,
+        desc: "Hiina keemialabori kartul(hind on kilo kohta).",
+        img: "imagelink"
+    },
+    {
+        id: "e",
+        name: "Portugali kartul",
+        price: 0.5,
+        desc: "Portugali tänava kartul(hind on kilo kohta).",
+        img: "imagelink"
+    }
+]
